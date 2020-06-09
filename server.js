6426088661b470ded902ee3d40a67c1bf6a3f7c3
Susan Ye <susanye3@gmail.com>
2020-06-08 23:06:27 -0700

    updating endpoints

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -35,8 +34,8 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     //testing purposes currently
-    if (req.body.email === database.user[0].email && req.body.password === database.users[0].password) {
-        res.json('success');
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -58,7 +57,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
